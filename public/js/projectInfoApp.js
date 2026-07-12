1426437fbdbfb6ca0d9248c8e3e512a4bc7434d9
Dhanush C K <dhanushck01@gmail.com>
2026-07-12 17:10:53 +0530

    feat: updated js file as per the new design

--- public/js/projectInfoApp.js
+++ public/js/projectInfoApp.js
@@ -4,3 +4,8 @@ tools.addEventListener('wheel', (event) => {
     event.preventDefault();
     tools.scrollLeft += event.deltaY;
 });
+
+const navEl = document.querySelector('.navigation-panel');
+window.addEventListener('scroll', ()=>{
+    navEl.classList.toggle('scrolled', window.scrollY > 20);
+});
